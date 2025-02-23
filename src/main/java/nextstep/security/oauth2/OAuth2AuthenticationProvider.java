e8f66982b297774fa771e7145cf40f7490139ed6
haero77 <shineecard5@gmail.com>
2025-02-23 09:58:18 +0900

    (step1-1) 깃헙에서 발급받은 액세스 토큰으로 유저 정보를 요청한다

--- src/main/java/nextstep/security/oauth2/OAuth2AuthenticationProvider.java
+++ src/main/java/nextstep/security/oauth2/OAuth2AuthenticationProvider.java
@@ -3,6 +3,8 @@ package nextstep.security.oauth2;
 import nextstep.security.authentication.Authentication;
 import nextstep.security.authentication.AuthenticationException;
 import nextstep.security.authentication.AuthenticationProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -13,9 +15,12 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
+import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
 
 public class OAuth2AuthenticationProvider implements AuthenticationProvider {
+
+    private static final Logger logger = LoggerFactory.getLogger(OAuth2AuthenticationProvider.class);
 
     private final RestTemplate restTemplate = new RestTemplate();
     private final SecurityOAuth2Properties securityProperties;
@@ -33,14 +38,15 @@ public class OAuth2AuthenticationProvider implements AuthenticationProvider {
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) authentication;
 
-        // Exchange code for an access token: Request POST https://github.com/login/oauth/access_token
+        // Step1: Exchange code for an access token
         AccessTokenResponse accessToken = exchangeCodeToAccessToken(token);
 
-        // todo request user info using access token
+        // Step2: Query user info from OAuth2 Provider using via access token
+        OAuth2UserInfo userInfo = getUserInfo(accessToken);
 
         // todo process after query user info from OAuth2 Provider: create member if not exists
 
-        // todo create OAuth2AuthenticationToken
+        // todo create authenticated OAuth2AuthenticationToken
         throw new UnsupportedOperationException();
     }
 
@@ -49,6 +55,9 @@ public class OAuth2AuthenticationProvider implements AuthenticationProvider {
         if (!accessToken.hasAccessToken()) {
             throw new AuthenticationException("Access token is empty");
         }
+
+        logger.info("Successfully received OAuth2 access token: {}", accessToken);
+
         return accessToken;
     }
 
@@ -73,6 +82,8 @@ public class OAuth2AuthenticationProvider implements AuthenticationProvider {
                 AccessTokenResponse.class
         );
 
+        logger.info("Access token response: {}", responseEntity);
+
         if (!responseEntity.getStatusCode().is2xxSuccessful()) {
             throw new AuthenticationException("Failed to get access token");
         }
@@ -82,5 +93,25 @@ public class OAuth2AuthenticationProvider implements AuthenticationProvider {
 
     private String generateGitHubAccessTokenRequestUrl() {
         return "%s/login/oauth/access_token".formatted(securityProperties.getGithub().domain());
+    }
+
+    private GitHubUserInfo getUserInfo(AccessTokenResponse accessToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        headers.setBearerAuth(accessToken.accessToken());
+        HttpEntity<Object> requestEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<GitHubUserInfo> responseEntity = this.restTemplate.exchange(
+                this.securityProperties.getGithub().apiDomain() + "/user", // GitHub email이 public이 아닐 경우 email이 null로 리턴
+                GET,
+                requestEntity,
+                GitHubUserInfo.class
+        );
+        logger.info("User info response: {}", responseEntity);
+
+        GitHubUserInfo userInfo = responseEntity.getBody();
+        logger.info("User info: {}", userInfo);
+
+        return userInfo;
     }
 }
---
+++ src/main/java/nextstep/security/oauth2/OAuth2UserInfo.java
@@ -0,0 +1,6 @@
+package nextstep.security.oauth2;
+
+public interface OAuth2UserInfo {
+
+    String getEmail();
+}
